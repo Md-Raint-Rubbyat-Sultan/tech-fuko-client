16e364a8cf0f20eaa64853bf886bae2c5ba31f9f
md ranit rubbyat sultan <mdranitrubbyatsultan@gmail.com>
2023-10-17 23:06:14 +0600

    clint side project setup is ready

---
+++ src/components/HelmetTitle/HelmetTitle.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet-async';
+
+
+const HelmetTitle = ({ title }) => {
+    return (
+        <Helmet>
+            <title>{title}</title>
+        </Helmet>
+    );
+}
+
+HelmetTitle.propTypes = {
+    title: PropTypes.string.isRequired,
+};
+
+export default HelmetTitle;
